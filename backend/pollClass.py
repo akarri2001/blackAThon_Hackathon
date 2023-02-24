c70c1f704e6c5ac598c06b7a4e74b0a6727240fa
Akash Karri <akash.r.karri@gmail.com>
2023-02-24 13:07:26 -0500

    Add functions on pollClass.py

--- backend/pollClass.py
+++ backend/pollClass.py
@@ -6,6 +6,11 @@ class OptionNotPresentError(Exception):
         self.message = f'ERROR: {optionString} not in pollId: {pollId}'
         super().__init__(self.message)
 
+class OptionAlreadyPresentError(Exception):
+    def __init__(self, optionString, pollId):
+        self.message = f'ERROR: {optionString} already in in pollId: {pollId}'
+        super().__init__(self.message)
+
 class TagNotPresentError(Exception):
     def __init__(self, tagString, pollId):
         self.message = f'ERROR: {tagString} not in pollId: {pollId}' 
@@ -43,5 +48,40 @@ class PollClass():
             self.tags.add(tagString)
             self.setNewLastEditTime()
     
+    def removeTag(self, tagString):
+        if tagString not in self.tags:
+            raise TagNotPresentError(tagString, self.uniqueId)
+        else:
+            self.tags.remove(tagString)
+            self.setNewLastEditTime()
+
+    def addOption(self, optionString):
+        if optionString in self.options:
+            raise OptionAlreadyPresentError(optionString, self.uniqueId)
+        else:
+            self.options[optionString] = 0
+            self.setNewLastEditTime()
     
+    def removeOption(self, optionString):
+        if optionString not in self.options:
+            raise OptionNotPresentError(optionString, self.uniqueId)
+        else:
+            del self.options[optionString]
+            self.setNewLastEditTime()
+        
+    def editQuestion(self, questionString):
+        if questionString != '':
+            self.questionString = questionString
+            self.setNewLastEditTime()
     
+    def getPoll_Id(self):
+        return self.uniqueId
+    
+    def getOptions(self):
+        return self.options
+    
+    def getTags(self):
+        return self.tags
+
+    def getCreatorId(self):
+        return self.creatorId
