1c7818b1bb5a3a1d70f89fc1b4069d15175e4473
DhruvSadhu <deltasigmara@gmail.com>
2023-02-25 08:04:34 -0500

    redoing implementation of roles in user classes

--- backend/app/server/models/sga_model.py
+++ backend/app/server/models/sga_model.py
@@ -1,6 +1,24 @@
 #how student data will be repsesented in MongoDB database
 from models.student_model import Student
 from pydantic import BaseModel
+from config.database import pollsCollection
+from config.database import sgaCollection
 
 class Sga(Student , BaseModel):
     pollsCreated: list
+    role:str = 'SGA'
+    
+    #remove an option from a poll 
+    def removeOption(poll_id, optionString):
+        print("placeholder")
+    #add an option to a poll 
+    def addOption(poll_id, optionString):
+        print("placeholder")
+    #add a tag to a poll
+    def addTag(poll_id, tagString):
+        print("")
+    #remove a tag from a poll
+    def removeTag(poll_id, tagString):
+        print("")
+    #edit question
+    
