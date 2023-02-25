64cc50f1a08f5400daa24672f928c406bf2f45af
Akash Karri <akash.r.karri@gmail.com>
2023-02-25 07:21:47 -0500

    Add Sidebar to gov and student profile pages

--- frontend/src/components/ProfilePageComponentGov.js
+++ frontend/src/components/ProfilePageComponentGov.js
@@ -3,6 +3,7 @@ import { Avatar } from "@mui/material";
 import { fontSize } from "@mui/system";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import SidebarGov from './SidebarGov';
 
 export default function ProfilePageComponentGov(props){
 
@@ -13,27 +14,36 @@ export default function ProfilePageComponentGov(props){
     let fullName = 'Akash Karri'
     let initials = firstName[0] + lastName[0]
     let email = 'arkarri@ncsu.edu'
-    return (
-        <div
-        style= {{
-            display: 'flex',
-            justifyContent:'center',
-            alignItems:'center',
-            height: '100vh',
-            flexDirection:'column',
-            gap: "10px",
-            border: "3px solid gray",
-            marginLeft: '20%',
-            marginRight: '20%'
-        }}>
-        <Avatar sx={{ height: '20vh', width:"auto" ,fontSize:"10em", background: "#CC0000", margin:'auto'}}>{initials}</Avatar>
-        <h1 style={{fontSize: "10vh", margin:'auto', textAlign: "center"}}>{fullName}</h1>
-        <h2 style={{fontSize: "2vh", margin:'auto', textAlign: "center"}}>{email}</h2>
-        <h2 style={{fontSize: "3vh", color:'gray', margin:'auto', textAlign: "center"}}>NC State University</h2>
-        <Button variant='outlined' style={{fontSize: "6vh", margin:'auto'}}>View My Polls</Button>
-        <Button variant='outlined' style={{fontSize: "6vh", margin:'auto'}} onClick = {() => {navigate('/pollTest')}}>See My Feed</Button>
-        <Button variant='outlined' style={{fontSize: "6vh", margin:'auto'}} onClick = {() => {navigate('/createPollTest')}}>Create a Poll</Button>
 
+    
+    return (
+        <div id="outer-container">
+            <SidebarGov pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+            <div id="page-wrap">
+                <div 
+                style= {{
+                    display: 'flex',
+                    justifyContent:'center',
+                    alignItems:'center',
+                    height: '100vh',
+                    flexDirection:'column',
+                    gap: "10px",
+                    border: "3px solid gray",
+                    marginLeft: '20%',
+                    marginRight: '20%'
+                }}>
+                <Avatar sx={{ height: '10vh', width:"auto" ,fontSize:"10em", background: "#CC0000", margin:'auto'}}>{initials}</Avatar>
+                <h1 style={{fontSize: "10vh", margin:'auto', textAlign: "center"}}>{fullName}</h1>
+                <h2 style={{fontSize: "2vh", margin:'auto', textAlign: "center"}}>{email}</h2>
+                <h2 style={{fontSize: "3vh", color:'gray', margin:'auto', textAlign: "center"}}>NC State University</h2>
+                <Button variant='outlined' style={{fontSize: "2vh", margin:'auto'}}>View My Polls</Button>
+                <Button variant='outlined' style={{fontSize: "2vh", margin:'auto'}} onClick = {() => {navigate('/pollTest')}}>See My Feed</Button>
+                <Button variant='outlined' style={{fontSize: "2vh", margin:'auto'}} onClick = {() => {navigate('/createPollTest')}}>Create a Poll</Button>
+            </div>
+        </div>
+
+
+       
         </div>
     )
     
