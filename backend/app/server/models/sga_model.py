cd3d42be891bffc22659ef1f1387b0522b02c609
DhruvSadhu <deltasigmara@gmail.com>
2023-02-25 09:00:08 -0500

    implementation of some methods in Sga class

--- backend/app/server/models/sga_model.py
+++ backend/app/server/models/sga_model.py
@@ -10,25 +10,27 @@ class Sga(Student , BaseModel):
     pollsCreated: list
     role:str = 'SGA'
     
+    def createPoll():
+        print("do i need this?")
     #remove an option from a poll 
     def removeOption(poll_id, optionString):
         #find the poll
         poll : PollClass = pollsCollection.find_one({"_id": ObjectId(poll_id)})
-        print("placeholder")
+        del poll.options[optionString]
     #add an option to a poll 
     def addOption(poll_id, optionString):
         #find the poll
         poll : PollClass = pollsCollection.find_one({"_id": ObjectId(poll_id)})
-        print("placeholder")
+        poll.options[optionString] = 0
     #add a tag to a poll
     def addTag(poll_id, tagString):
         #find the poll
         poll : PollClass = pollsCollection.find_one({"_id": ObjectId(poll_id)})
-        print("")
+        poll.addTag(tagString)
     #remove a tag from a poll
     def removeTag(poll_id, tagString):
         #find the poll
         poll : PollClass = pollsCollection.find_one({"_id": ObjectId(poll_id)})
-        print("")
+        poll.removeTag(tagString)
     #edit question
     
