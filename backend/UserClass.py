525ba0bff3379b73077e84b408bb223304221f05
DhruvSadhu <deltasigmara@gmail.com>
2023-02-25 07:38:04 -0500

    requirements save

--- backend/UserClass.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC
-
-class UserClass(ABC):
-    def __init__(self, fullNameString, uniqueIdString, usernameString, passwordString):
-        self.fullNameString = fullNameString
-        self.uniqueIdString = uniqueIdString
-        self.usernameString = usernameString
-        self.passwordString = passwordString
-
