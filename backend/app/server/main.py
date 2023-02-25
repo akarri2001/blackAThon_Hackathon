1b9a7ab2f40ebf80f5e166ea50a12a954cb6610f
Akash Karri <akash.r.karri@gmail.com>
2023-02-25 09:08:04 -0500

    Add CORS policy and initial request

--- backend/app/server/main.py
+++ backend/app/server/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from routes.student_routes import student_api_router
 from routes.poll_routes import poll_api_router
 from routes.sga_routes import sga_api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #students api
 app.include_router(student_api_router)
